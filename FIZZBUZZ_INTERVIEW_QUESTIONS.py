f7ba834346535b74480705db46e93b8bcdc2faa3
Reema_abde <107676935+Remmaabde@users.noreply.github.com>
2023-09-11 18:00:29 +0300

    Add files via upload

---
+++ FIZZBUZZ_INTERVIEW_QUESTIONS.py
@@ -0,0 +1,12 @@
+for i in range(1,101):
+    if(i%3==0):
+        print("fIZZ")
+    elif(i%5==0):
+        print("BUZZ")
+    elif(i%3==0 and i%5==0):
+        print("FIZZBUZZ")
+    else:
+        print(i)
+
+    
+    
